5cf4690935a6e4e0643446bbbc9c33b3e4819841
hisamaszaini <muhammadhisamaszaini@gmail.com>
2025-08-31 19:26:02 +0700

    Remove logActivity utils

--- src/utils/logActivity.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogActivityService } from './logActivity';
-
-@Module({
-  providers: [LogActivityService],
-  exports: [LogActivityService],
-})
-export class LogActivityModule {}
